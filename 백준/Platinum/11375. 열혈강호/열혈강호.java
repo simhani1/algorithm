22342e8c47e17aa9ba64d52be6e2de532b5a6231
Jonghan Sim <simhani1@gmail.com>
2024-12-20 02:52:14 +0900

    [Platinum IV] Title: 열혈강호, Time: 604 ms, Memory: 82720 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static int N, M;
+    private static int[][] arr = new int[1001][];
+    private static int[] managers = new int[1001];
+    private static boolean[] visited = new boolean[1001];
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int cnt = Integer.parseInt(st.nextToken());
+            arr[i] = new int[cnt];
+            for (int j = 0; j < cnt; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            if (dfs(i)) {
+                ans++;
+            }
+            Arrays.fill(visited, false);
+        }
+        System.out.println(ans);
+    }
+
+    private static boolean dfs(int person) {
+        for (int i = 0; i < arr[person].length; i++) {
+            int task = arr[person][i];
+            if (visited[task]) {
+                continue;
+            }
+            visited[task] = true;
+            if (managers[task] == 0 || dfs(managers[task])) {
+                managers[task] = person;
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
